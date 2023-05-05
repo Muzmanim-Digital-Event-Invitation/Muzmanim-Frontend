9f28f0770850dcb834eb83021f74102252759aaa
coilwhine <hen.daniel47@gmail.com>
2023-05-05 15:38:47 +0300

    header basics

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,29 @@
+import { NavLink } from "react-router-dom";
 import "./Header.scss";
 
+
 function Header(): JSX.Element {
+
     return (
-        <div className="Header">
-            <h3>header</h3>
-        </div>
+        <header className="Header">
+            <div className="container">
+                <div className="auth-section">
+                    <div className="profile-img"></div>
+                    <span className="profile-name">דניאל חן</span>
+                </div>
+                <nav className="nav-section">
+                    <NavLink to={"/"}>בית</NavLink>
+                    <NavLink to={"/"}>בדיקה אחרת</NavLink>
+                    <NavLink to={"/"}>סתם מילה</NavLink>
+                </nav>
+                <div className="logo-section">
+                    <span className="logo-text">
+                        מוזמנים
+                    </span>
+                    <div className="logo"></div>
+                </div>
+            </div>
+        </header>
     );
 }
 
