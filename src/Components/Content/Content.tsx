389b7ff371f790fb89f96b2d28d30ad5708fceff
Shalev140 <112413822+Shalev140@users.noreply.github.com>
2023-05-05 16:35:53 +0300

    create home page component

--- src/Components/Content/Content.tsx
+++ src/Components/Content/Content.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./Content.scss";
+import HomePage from "./HomePage/HomePage";
 
 function Content(): JSX.Element {
     return (
         <div className="Content">
             <Routes>
-                <Route path="/" element={<div>Home</div>} />
+                <Route path="/" element={<HomePage/>} />
             </Routes>
         </div>
     );
