cb6420103d6f385b2b774bf72d2a032004d732a3
coilwhine <hen.daniel47@gmail.com>
2023-05-12 18:33:42 +0300

    invite

--- src/Components/Content/NewEvent/NewEvent.tsx
+++ src/Components/Content/NewEvent/NewEvent.tsx
@@ -4,30 +4,29 @@ import { useState } from "react";
 import EventInfo from "./EventInfo/EventInfo";
 import EventDesign from "./EventDesign/EventDesign";
 import EventType from "./EventType/EventType";
-import React from "react";
 
 function NewEvent(): JSX.Element {
-    const [stepNumber, setStepNumber] = useState<number>(0);
-
-    
-
-    return (
-      <div className="NewEvent">
-        <h1>test</h1>
-        <CustomizedSteppers stepNumber={stepNumber} />
-        {stepNumber === 0 ? 
-            <EventType  stepNumber={stepNumber}  setStepNumber={setStepNumber}/>
-        :<></>}
-        {stepNumber === 1 ? 
-            <EventInfo  stepNumber={stepNumber}  setStepNumber={setStepNumber}/>
-        :<></>}
-        {stepNumber === 2 ? 
-            <EventDesign  stepNumber={stepNumber}  setStepNumber={setStepNumber}/>
-        :<></>}
+  const [stepNumber, setStepNumber] = useState<number>(0);
 
 
-      </div>
-    );
+
+  return (
+    <div className="NewEvent">
+      <h1>test</h1>
+      <CustomizedSteppers stepNumber={stepNumber} />
+      {stepNumber === 2 ?
+        <EventType stepNumber={stepNumber} setStepNumber={setStepNumber} />
+        : <></>}
+      {stepNumber === 1 ?
+        <EventInfo stepNumber={stepNumber} setStepNumber={setStepNumber} />
+        : <></>}
+      {stepNumber === 0 ?
+        <EventDesign stepNumber={stepNumber} setStepNumber={setStepNumber} />
+        : <></>}
+
+
+    </div>
+  );
 }
 
 export default NewEvent;
