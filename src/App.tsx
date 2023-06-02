7962c8fb1d8d5232ad8837723eb2dd1a5e930d06
harelsarag7 <112413505+harelsarag7@users.noreply.github.com>
2023-06-02 12:41:00 +0300

    disabled header footer in event for guest

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import './App.scss'
-import Content from './Components/Content/Content'
-import Footer from './Components/Footer/Footer'
-import Header from './Components/Header/Header'
+import { useLocation } from 'react-router-dom';
+import './App.scss';
+import Content from './Components/Content/Content';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 
 function App() {
+  const location = useLocation();
+  const hideHeaderFooterRoutes = ['/event/'];
+
+  const shouldHideHeaderFooter = hideHeaderFooterRoutes.some((route) =>
+    location.pathname.startsWith(route)
+  );
 
   return (
     <div className='App'>
-      <Header />
+      {!shouldHideHeaderFooter && <Header />}
       <Content />
-      <Footer />
+      {!shouldHideHeaderFooter && <Footer />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
